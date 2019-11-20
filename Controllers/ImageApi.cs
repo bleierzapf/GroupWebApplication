0492b356907837309528556c81980cbd9530e489
Brian Leierzapf <bleierzapf@me.com>
2019-11-19 19:30:39 -0800

    Some updated stuff. Attached to Azure DB and image table.

--- Controllers/ImageApi.cs
+++ Controllers/ImageApi.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -9,7 +10,47 @@ namespace GroupWebApplication.Controllers
 {
     public class ImageApi
     {
-        public static async Task<ImageModel> GetImageAsync(ImageModel imageModel, String path)
+        public static ImageModel DailyImage = new ImageModel();
+
+        public static async Task GetDailyImage()
+        {
+            String dailyPath = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+            try
+            {
+                DailyImage = await GetImageAsync(DailyImage, dailyPath);
+                DailyImageToDataTable(DailyImage);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+
+        private static void DailyImageToDataTable(ImageModel dailyImage)
+        {
+            DataTable imageTable = new DataTable();
+            imageTable.Columns.Add("Pk", typeof(int));
+            imageTable.Columns.Add("Date", typeof(DateTime));
+            imageTable.Columns.Add("Explanation", typeof(String));
+            imageTable.Columns.Add("Media_Type", typeof(String));
+            imageTable.Columns.Add("Title", typeof(String));
+            imageTable.Columns.Add("Url", typeof(String));
+            imageTable.Columns.Add("Hdurl", typeof(String));
+
+            DataRow dr = imageTable.NewRow();
+            dr["Pk"] = 0;
+            dr["Date"] = dailyImage.Date;
+            dr["Explanation"] = dailyImage.Explanation;
+            dr["Media_Type"] = dailyImage.Media_Type;
+            dr["Title"] = dailyImage.Title;
+            dr["Url"] = dailyImage.Url;
+            dr["Hdurl"] = dailyImage.Hdurl;
+            
+            Console.WriteLine("From Table " + dailyImage.Url);
+        }
+        
+        static async Task<ImageModel> GetImageAsync(ImageModel imageModel, String path)
         {
             HttpClient client = new HttpClient();
 
@@ -18,7 +59,7 @@ namespace GroupWebApplication.Controllers
             return imageModel;
         }
         
-        //write to concole for testing
+        //write to console for testing
         public static void ShowImageModel(ImageModel imageModel)
         {
             Console.WriteLine($"Date: {imageModel.Date}\n" +
--- Program.cs
+++ Program.cs
@@ -15,30 +15,13 @@ namespace GroupWebApplication
     {
         public static void Main(string[] args)
         {
+            Controllers.ImageApi.GetDailyImage();
             CreateHostBuilder(args).Build().Run();
-            GetDailyImage();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-        
-        static async Task GetDailyImage()
-        {
-            String dailyPath = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
-            ImageModel dailyImage = new ImageModel();
-            try
-            {
-                dailyImage = await Controllers.ImageApi.GetImageAsync(dailyImage, dailyPath);
-                Controllers.ImageApi.ShowImageModel(dailyImage);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.ReadLine();
 
-        }
-        
     }
 }
