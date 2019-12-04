f254c4c62d353731f1711849b433b23fca145fd5
Andrew Franko <andrewhfranko@gmail.com>
2019-12-03 19:29:03 -0800

    Added API

---
+++ API/ImageModelsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GroupWebApplication.Data;
+using GroupWebApplication.Models;
+
+namespace GroupWebApplication.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageModelsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ImageModelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ImageModels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ImageModel>>> GetImageModel()
+        {
+            return await _context.ImageModel.ToListAsync();
+        }
+
+        // GET: api/ImageModels/5
+        [HttpGet("{Date}")]
+        public async Task<ActionResult<ImageModel>> GetImageModel(string Date)
+        {
+            var imageModel = await _context.ImageModel.FindAsync(Date);
+
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+
+            return imageModel;
+        }
+
+        private bool ImageModelExists(string Date)
+        {
+            return _context.ImageModel.Any(e => e.Date == Date);
+        }
+    }
+}
